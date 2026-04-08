612b6870e6fa0b7c144bd753c12380752835870a
Nicolas <nicolasgabriel321@gmail.com>
2026-04-08 20:16:46 -0300

    Adicionado ao sistema a opcao de logar com 3 tipos de usuarios diferentes, e a tambem adicionado menus para o recepcionista, o administrador e o auxiliar de limpeza

--- sistema-hoteleiro/main.c
+++ sistema-hoteleiro/main.c
@@ -1,80 +1,158 @@
-#include <stdio.h>
-#include <string.h>
-#include <windows.h>
+#include <stdio.h> 
+#include <stdlib.h> 
+#include <string.h> // Essa biblioteca serve para usar os structs e manipulação de strings
 
-// Definições de cores para o terminal
-#define RED     "\x1b[31m"
-#define GREEN   "\x1b[32m"
-#define YELLOW  "\x1b[33m"
-#define BLUE    "\x1b[34m"
-#define RESET   "\x1b[0m"
+// STRUCT DE LOGIN NO SISTEMA
 
-int main() {
-    // Configura a saída para UTF-8 (para acentos)
-    SetConsoleOutputCP(65001);
+typedef struct{
+    char login[30];
+    char senha[30];
+} pessoa; pessoa p[5];  // diminuindo o nome da struct para "p" e o "[1]" é o máximo de pessoas com logins e senhas
 
+// MENU RECEPCIONISTA
+
+void menuRecepcionista() {
+    int opcao;
+    printf("\n==========================");
+    printf("\n=== PAINEL DA RECEPCAO ===");
+    printf("\n==========================\n");
+    printf("1. Cadastro de Hospedes\n");
+    printf("2. Controle de Quartos\n");
+    printf("3. Reservas Feitas\n");
+    printf("4. Check-in\n");
+    printf("5. Check Out\n");
+    printf("Escolha uma opcao: ");
+    scanf("%d", &opcao);
+
+
+    switch (opcao){
+    case 1:
+    printf("\nCadastro de Hospedes selecionado");
+        break;
+    
+    default:
+        printf("Numero invalido, tente novamente.");
+        break;
+    }
+}
+
+
+// MENU ADMINISTRADOR
+
+void menuAdministrador(){
+    int opcao;
+    printf("\n===============================");
+    printf("\n=== PAINEL DO ADMINISTRADOR ===");
+    printf("\n===============================");
+    printf("\n1. ");
+    printf("\n2. ");
+    printf("\n3. ");
+    printf("\n4. ");
+    printf("\n5. ");
+    printf("\nEscolha uma opcao: ");
+
+    scanf("%d", &opcao);
+
+    switch (opcao)
+    {
+    case 1:
+        /* code */
+        break;
+    
+    default:
+        break;
+    }
+}
+
+// MENU AUXILIAR DE LIMPEZA
+
+void menuAuxiliarDeLimpeza(){
+    printf("\n=====================================");
+    printf("\n=== PAINEL DO AUXILIAR DE LIMPEZA ===");
+    printf("\n=====================================");
+    printf("\n1. ");
+    printf("\n2. ");
+    printf("\n3. ");
+    printf("\n4. ");
+    printf("\n5. ");
+    printf("Escolha uma opcao: ");
     int opcao;
 
-    do {
-        printf(GREEN"\n===================================");
-        printf(GREEN"\n===================================");
-        printf(GREEN"\n=== SISTEMA DE GESTAO HOTELEIRA ===");
-        printf(GREEN"\n===================================");
-        printf(GREEN"\n===================================\n"RESET);
+    scanf("%d", &opcao);
+
+    switch (opcao)
+    {
+    case 1:
+        /* code */
+        break;
+    
+    default:
+        break;
+    }
+}
 
 
-        printf("\n1. Cadastro de Hóspedes");
-        printf("\n2. Controle de Quartos");
-        printf("\n3. Registro de Reservas");
-        printf("\n4. Check-in e Check-out");
-        printf("\n5. Processamento de Pagamentos");
-        printf("\n6. Emissão de Documentos");
-        printf("\n7. Atualização de Limpeza");
-        printf("\n8. Relatórios Gerenciais");
-        printf("\n0. Sair");
-        printf("\nEscolha uma opcao: ");
-        scanf("%d", &opcao);
+int main(){
+    
+// ====== SISTEMA DE LOGIN ======
 
-        switch(opcao) {
-            case 1:
-                printf("Cadastro de Hóspedes\n");
-                system("pause");
-                break;
-            case 2:
-                printf("Controle de Quartos\n");
-                system("pause");
-                break;
-            case 3:
-                printf("Registro de Reservas\n");
-                system("pause");
-                break;
-            case 4:
-                printf("Check-in e Check-out\n");
-                system("pause");
-                break;
-            case 5:
-                printf("Processamento de Pagamentos\n");
-                system("pause");
-                break;
-            case 6:
-                printf("Emissão de Documentos\n");
-                system("pause");
-                break;
-            case 7:
-                printf("Atualização de Limpeza\n");
-                system("pause");
-                break;
-            case 8:
-                printf("Relatórios Gerenciais\n");
-                system("pause");
-                break;
-            case 0:
-                printf("Encerrando...\n");
-                break;
-            default:
-                printf("Opcao invalida!\n");
-        }
-    } while(opcao != 0);
+    char login[30]; // responsável por armazenar a senha inserida pelo usuário
+    char senha[30];
+
+    int tentativas = 0;
+    int logado = 0;
+
+//   ====== USUARIOS ======
+    // Usuario da recepcao
+    strcpy(p[0].login, "Recepcionista");
+    strcpy(p[0].senha, "recepcao123");
+    // Usuario da administracao
+    strcpy(p[1].login, "Admin"); 
+    strcpy(p[1].senha, "admin123"); 
+    // Usuario da equipe de limpeza
+    strcpy(p[2].login, "Limpeza"); 
+    strcpy(p[2].senha, "limpeza123"); 
+
+    while (tentativas < 3 && logado == 0){
+        printf("\n===================================");
+        printf("\n=== SISTEMA DE GESTAO HOTELEIRA ===");
+        printf("\n===================================");
+        printf("\nLogin: ");
+        scanf("%s", login); // armazenando os dados inseridos pelo usuário para o vetor login que está dentro da função main
+        printf("\nSenha: ");
+        scanf("%s", senha);
+
+        if ((strcmp(login,p[0].login)==0) && (strcmp(senha,p[0].senha)==0)){ // A função strcmp é responsável por comparar string com string
+        // RECEPCIONISTA LOGADO
+            printf("\nBem-vindo(a), Recepcionista!\n");
+            logado = 1;
+            menuRecepcionista();
+        } else if((strcmp(login,p[1].login)==0) && (strcmp(senha,p[1].senha)==0)) {
+
+        // ADMIN LOGADO
+            printf("\nBem-vindo(a), Administrador!\n");
+            logado = 1;
+            menuAdministrador();
+        } else if((strcmp(login,p[2].login)==0) && (strcmp(senha,p[2].senha)==0)) {
+
+        // EQUIPE DE LIMPEZA LOGADO
+            printf("\nBem-vindo(a), Auxiliar de limpeza!\n");
+            logado = 1;
+            menuAuxiliarDeLimpeza();
+        } 
+        else{
+           tentativas++;
+           printf("\nErro: Credenciais incorretas.\n");
+           if (tentativas < 3) {
+            printf("Voce tem mais %d tentativa(s).\n", 3 - tentativas);
+           } else {
+            printf("Acesso bloqueado por seguranca!\n");
+           }
+    }
+    }
+
+
+    
 
     return 0;
 }
